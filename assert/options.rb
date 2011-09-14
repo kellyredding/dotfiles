b1fa093b73b2a93044465309dee157dc2f492bce
Kelly <kelly@kelredd.com>
2011-09-14 04:25:41 -0500

    updated assert options and minor README tweak

--- assert/options.rb
+++ assert/options.rb
@@ -1,1 +1,5 @@
-Assert::Test.options.capture_output true
+Assert::Test.options.capture_output  true
+Assert.options.view.options.styled   true
+
+# default view setting
+Assert.options.view                  Assert::View::DefaultView.new($stdout)
