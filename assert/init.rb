214fe503bdb784bf76d9dd9f3f09152e34b6c4f2
Kelly Redding <kelly@kellyredding.com>
2015-09-18 17:33:41 -0500

    remove setting custom assert view
    Not really needed since all I was doing was setting the default
    value.

--- assert/init.rb
+++ assert/init.rb
@@ -1,7 +1,6 @@
 Assert.configure do |c|
   c.capture_output false
   c.halt_on_fail   false
-  c.view           Assert::View::DefaultView.new($stdout)
 end
 
 # TODO: rework for new config/init pattern
