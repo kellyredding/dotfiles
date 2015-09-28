3171e66a03cec5b610a234adbf763e75d774f12f
Kelly Redding <kelly@kellyredding.com>
2015-09-28 14:21:00 -0500

    gitconfig: reorg git diff scripts as bins
    This allows me to run the pager without the `-l` bash opt to speed
    things up a bit.  This also tweaks the colors to taste a bit more
    now that I've used this diff output for a while now.

--- bash/git.sh
+++ bash/git.sh
@@ -12,21 +12,3 @@ fi
 export GIT_PS1_SHOWDIRTYSTATE=true
 export GIT_PS1_SHOWSTASHSTATE=true
 export GIT_PS1_SHOWUPSTREAM=true
-
-# git diff utils
-
-function git_strip_diff_leading_symbols() {
-  color_code_regex="(\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K])"
-
-  # simplify the unified patch diff header
-  gsed -r "s/^($color_code_regex)diff --git .*$//g" | \
-    gsed -r "s/^($color_code_regex)index .*$/\n\1$(git_horz_rule)/g" | \
-    gsed -r "s/^($color_code_regex)\+\+\+(.*)$/\1+++\5\n\1$(git_horz_rule)\x1B\[m/g" | \
-
-  # actually strips the leading symbols
-    gsed -r "s/^($color_code_regex)[\+\-]/\1 /g"
-}
-
-function git_horz_rule() {
-  printf "%$(tput cols)s\n"|tr " " "─"
-}
